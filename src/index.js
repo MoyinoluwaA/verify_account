7ae1b2e86d60b77ffe5acdb2e41b17ee0fbf6a87
Moyinoluwa Adenuga <dayoh14@gmail.com>
2021-11-22 00:46:08 +0100

    added cors header to server

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const router = require('./routes')
 const db = require('./db')
+const cors = require('cors')
 
 const app = express()
 const port = process.env.PORT || 4000
 
+
+app.use(cors({
+    origin: '*'
+}))
 app.use(express.json())
 app.use(express.urlencoded({
     extended: true
