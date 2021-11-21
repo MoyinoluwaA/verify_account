d44b5fff1b0263fb994d5152f2ab00982746f90f
Moyinoluwa Adenuga <dayoh14@gmail.com>
2021-11-21 17:49:27 +0100

    moved db online

--- src/db/index.js
+++ src/db/index.js
@@ -3,18 +3,8 @@ const pgp = require('pg-promise')
 const promise = require('bluebird')
 
 dotenv.config()
-const { DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD } = process.env
-
-const cn = {
-    host: 'localhost',
-    port: 5432,
-    database: DATABASE_NAME,
-    user: DATABASE_USER,
-    password: DATABASE_PASSWORD,
-    max: 30 
-}
 
 const pg = pgp({ promiseLib: promise, noWarnings: true })
-const db = pg(cn)
+const db = pg(process.env.DATABASE_URL)
 
 module.exports = db
